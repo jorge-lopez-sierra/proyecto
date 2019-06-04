e037ee88f441fd535ef1aa0e62d725df96836392
jorge-lopez-sierra <48024029+jorge-lopez-sierra@users.noreply.github.com>
2019-06-04 11:38:12 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function Confirmar() {
+
+var mensaje = confirm("¿Desea Guaradar su usuario y contraseña?");
+if (mensaje) {
+alert("Su usuario y contraseña se han guardado");
+window.open(proyecto/index.html);
+}else
+	window.open(proyecto/index.html)
+}
